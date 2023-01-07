b3579af5694a43c6fc78b9c45a620d27a1eaff08
dprabhat670 <111510783+dprabhat670@users.noreply.github.com>
2023-01-07 17:45:29 +0530

    h

--- src/components/subcomp/Reelfooter.jsx
+++ src/components/subcomp/Reelfooter.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import {BiMusic} from 'react-icons/bi'
 function Reelfooter() {
   return (
-    <div id="reelfooter" className='relative bottom-10 bg-transparent text-white'>
-      Hello this is fo0ter ! 
+    <div id="reelfooter" className='relative bottom-[77px] px-2 bg-transparent text-white flex flex-col'>
+      <h3 className='absolute left-2
+       bg-transparent'>@username</h3>
+      <p className='
+      absolute left-2 -bottom-8 bg-transparent'>this is a description ! </p>
+      <BiMusic className='absolute -bottom-16 bg-transparent font-bold'/>
     </div>
   )
 }
