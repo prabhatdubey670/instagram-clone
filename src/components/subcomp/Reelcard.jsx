9512e5d5f2cce02a76af3f982d0316e5cdd0fe8d
dprabhat670 <111510783+dprabhat670@users.noreply.github.com>
2023-03-02 23:15:53 +0530

    firebase error resolved !

--- src/components/subcomp/Reelcard.jsx
+++ src/components/subcomp/Reelcard.jsx
@@ -1,41 +1,37 @@
-import React ,{useRef, useState} from 'react'
-import Reelsidebar from './Reelsidebar';
-import Reelfooter from './Reelfooter';
+import React, { useRef, useState } from "react";
+import Reelsidebar from "./Reelsidebar";
+import Reelfooter from "./Reelfooter";
 
-
-function Reelcard({
-  url,username,caption,music,likes,comments,shares
-}) {
-  const [playing,setPlaying] = useState(false);
+function Reelcard({ url, username, caption, music, likes, comments, shares }) {
+  const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
-  const onVideoPress =()=>{
-    if(playing){
+  const onVideoPress = () => {
+    if (playing) {
       videoRef.current.pause();
       setPlaying(false);
-    }
-    else{
+    } else {
       videoRef.current.play();
       setPlaying(true);
     }
-    
-  }
+  };
 
   return (
     <>
-    
-    <div className='md:w-full md:h-full snap-y overflow-hidden md:relative md:snap-start md:border-none  md:bg-white md:object-fill top-[50px] relative' >
-      <video src={url} loop
-      ref={videoRef}
-      onClick={onVideoPress}
-      key={url} className=""/>
-      <Reelfooter username={username} music={music} caption={caption
-      } />
-      
-      <Reelsidebar likes={likes} comments={comments} shares={shares}/>
-    </div>
+      <div className="md:w-full md:h-full snap-y overflow-hidden md:relative md:snap-start md:border-none  md:bg-white md:object-fill top-[50px] relative">
+        <video
+          src={url}
+          loop
+          ref={videoRef}
+          onClick={onVideoPress}
+          key={url}
+          className=""
+        />
+        <Reelfooter username={username} music={music} caption={caption} />
 
+        <Reelsidebar likes={likes} comments={comments} shares={shares} />
+      </div>
     </>
-  )
+  );
 }
 
-export default Reelcard
+export default Reelcard;
